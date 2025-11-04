f1d49d3728abedeb680e8172d51912ac40c6ca36
patrickarias078-droid <patrickarias078@gmail.com>
2025-11-04 12:35:07 -0500

    Save project changes, add demo and docs images for GitHub Pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('mi-app');
+  protected readonly title = signal('proyecto');
 }
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+  imports: [CommonModule],
+})
+export class Home {
+  slides = [
+    { src: '/assets/imagen/lomo.jpg', title: 'Lomo Saltado' },
+    { src: '/assets/imagen/ceviche.jpg', title: 'Ceviche' },
+    { src: '/assets/imagen/chaufa.jpg', title: 'Arroz Chaufa' }
+  ];
+
+  current = 0;
+
+  next(){ this.current = (this.current + 1) % this.slides.length; }
+  prev(){ this.current = (this.current - 1 + this.slides.length) % this.slides.length; }
+  go(i:number){ this.current = i; }
+}
